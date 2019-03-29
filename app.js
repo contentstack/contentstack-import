8ed583ab79d36afe93ad58cfd8fe6d0546dbab08
iyerrama29 <iyerrama29+contentstack.github@gmail.com>
2019-03-29 12:24:06 +0530

    npm ignore, package.json updated

--- app.js
+++ app.js
@@ -53,7 +53,6 @@ login(config).then(function () {
         log.success('Import utility executed succesfully!');
         return;
       }).catch(function (error) {
-        console.error(error)
         log.error('Import utility failed while executing');
         log.error(error);
         return;
