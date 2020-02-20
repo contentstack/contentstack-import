dab3e8c98d4fd08ad43d54bc662c17a267c84b18
Rohit Mishra <rohit.mishra@contentstack.com>
2020-02-20 15:03:56 +0530

    updated the config input style used cli for config input

--- app.js
+++ app.js
@@ -8,71 +8,89 @@ var ncp = require('ncp');
 var Bluebird = require('bluebird');
 var fs = require('fs');
 var path = require('path');
-
-var util = require('./lib/util/index');
+const { prompt } = require('inquirer');
 var login = require('./lib/util/login');
 var log = require('./lib/util/log');
 var config = require('./config');
+const stack = require('./lib/import/create_stack');
+var util = require('./lib/util/index');
 
-config = util.buildAppConfig(config);
-util.validateConfig(config);
 
-exports.getConfig = function () {
-  return config;
-};
+const loginQuestions = [
+  {
+    type: 'input',
+    name: 'email',
+    message: 'Enter email address:',
+    validate: (emailInput) => {
+      var regExp = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+      if (!regExp.test(emailInput)) {
+        return 'Email is not valid';
+      }
+      return true;
+    }
+  },
+  {
+    type: 'password',
+    name: 'password',
+    mask: '*',
+    message: 'Enter your password:'
+  }
+];
 
-login(config)
-  .then(function () {
-    var migrationBackupDirPath = path.join(process.cwd(), '_backup_' + Math.floor((Math.random() * 1000)));
-    return createBackup(migrationBackupDirPath).then((basePath) => {
-      config.data = basePath;
-      return util.sanitizeStack(config);
-    })
-      .then(() => {
-        var types = config.modules.types;
-
-        if (process.argv.length === 3) {
-          var val = process.argv[2];
-          if (val && types.indexOf(val) > -1) {
-            var moduleImport = require('./lib/import/' + val);
-            return moduleImport.start().then(function () {
-              log.success(val + ' was imported successfully!');
-              return;
-            }).catch(function (error) {
-              log.error('Failed to import ' + val);
-              log.error(error);
-              return;
-            });
-          } else {
-            log.error('Please provide valid module name.');
-            return 0;
-          }
-        } else if (process.argv.length === 2) {
-          var counter = 0;
-          return Bluebird.map(types, function () {
-            var importModule = require('./lib/import/' + types[counter]);
-            counter++;
-            return importModule.start();
-          }, {
-            concurrency: 1
-          }).then(function () {
-            log.success('Import utility executed succesfully!');
-            return;
+prompt(loginQuestions).then(loginAnswers => {
+  login(config, loginAnswers)
+    .then(function (config) {
+      stack().then(() => {
+        var migrationBackupDirPath = path.join(process.cwd(), '_backup_' + Math.floor((Math.random() * 1000)));
+        return createBackup(migrationBackupDirPath).then((basePath) => {
+          config.data = basePath;
+          return util.sanitizeStack(config);
+        })
+          .then(() => {
+            var types = config.modules.types;
+            if (process.argv.length === 3) {
+              var val = process.argv[2];
+              if (val && types.indexOf(val) > -1) {
+                var moduleImport = require('./lib/import/' + val);
+                return moduleImport.start().then(function () {
+                  log.success(val + ' was imported successfully!');
+                  return;
+                }).catch(function (error) {
+                  log.error('Failed to import ' + val);
+                  log.error(error);
+                  return;
+                });
+              } else {
+                log.error('Please provide valid module name.');
+                return 0;
+              }
+            } else if (process.argv.length === 2) {
+              var counter = 0;
+              return Bluebird.map(types, function () {
+                var importModule = require('./lib/import/' + types[counter]);
+                counter++;
+                return importModule.start();
+              }, {
+                concurrency: 1
+              }).then(function () {
+                log.success('Import utility executed succesfully!');
+                return;
+              }).catch(function (error) {
+                log.error('Import utility failed while executing');
+                log.error(error);
+                return;
+              });
+            } else {
+              log.error('Only one module can be exported at a time.');
+              return 0;
+            }
           }).catch(function (error) {
-            console.error(error)
-            log.error('Import utility failed while executing');
-            log.error(error);
-            return;
+            log.error((error.message) ? error.message: error);
+            process.exit(1);
           });
-        } else {
-          log.error('Only one module can be exported at a time.');
-          return 0;
-        }
-      }).catch(function (error) {
-        log.error((error.message) ? error.message: error);
-        process.exit(1);
-      });
-  });
+     });
+   });
+});
 
 function createBackup (backupDirPath) {
   return new Promise(function (resolve, reject) {
@@ -98,3 +116,7 @@ function createBackup (backupDirPath) {
     }
   });
 }
+
+//exports.login = login;
+
+
--- config/index.js
+++ config/index.js
@@ -1,13 +1,85 @@
+/* eslint-disable no-dupe-keys */
 module.exports = {
+  versioning: false,
+  host: 'https://api.contentstack.io/v3',
+  modules: {
+    types: [
+      'assets',
+      'locales',
+      'environments',
+      'extensions',
+      'webhooks',
+      'global_fields',
+      'content_types',
+      'entries',
+      'labels'
+    ],
+    locales: {
+      dirName: 'locales',
+      fileName: 'locales.json',
+      requiredKeys: [Array]
+    },
+    environments: { dirName: 'environments', fileName: 'environments.json' },
+    labels: { dirName: 'labels', fileName: 'labels.json' },
+    extensions: { dirName: 'extensions', fileName: 'extensions.json' },
+    webhooks: { dirName: 'webhooks', fileName: 'webhooks.json' },
+    assets: {
+      dirName: 'assets',
+      fileName: 'assets.json',
+      limit: 100,
+      host: 'https://api.contentstack.io',
+      validKeys: [Array],
+      assetBatchLimit: 5
+    },
+    content_types: {
+      dirName: 'content_types',
+      fileName: 'content_types.json',
+      validKeys: [Array],
+      limit: 100
+    },
+    entries: {
+      dirName: 'entries',
+      fileName: 'entries.json',
+      invalidKeys: [Array],
+      limit: 50,
+      assetBatchLimit: 5
+    },
+    globalfields: {
+      dirName: 'global_fields',
+      fileName: 'globalfields.json',
+      validKeys: [Array],
+      limit: 100
+    },
+    stack: { dirName: 'stack', fileName: 'stack.json' }
+  },
+  apis: {
+    userSession: '/user-session/',
+    locales: '/locales/',
+    environments: '/environments/',
+    assets: '/assets/',
+    content_types: '/content_types/',
+    entries: '/entries/',
+    extensions: '/extensions/',
+    webhooks: '/webhooks/',
+    globalfields: '/global_fields/',
+    folders: '/folders/',
+    stacks: '/stacks/',
+    labels: '/labels/'
+  },
+  preserveStackVersion: false,
+  configUrl: '',
+  authtoken: '***REMOVED***',
+  headers: {
+    authtoken: '***REMOVED***',
+    api_key:'blt8f3e0aa94fe4930b',
+    access_token: 'bltba631263d6472cb1',
+    'X-User-Agent': 'contentstack-import/v1.5.0'
+  },
+  useBackedupDir: '',
   master_locale: {
     // master locale of the stack
     name: 'English - United States',
     code: 'en-us'
   },
-  // Credentials
-  email: '',
-  password: '',
-  target_stack: '',
-  // Folder in which exported contents are stored
-  data: './contents'
+  data:'./contents_new'
 };
