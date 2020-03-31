1ef89f3a82b1646afa2f74cafb21f92bff0931e3
Rohit Mishra <rohit.mishra@contentstack.com>
2020-03-31 12:21:01 +0530

    Added feature under globalfield

--- config/index.js
+++ config/index.js
@@ -5,9 +5,9 @@ module.exports = {
     code: 'en-us'
   },
   // Credentials
-  email: 'rohit.mishra@contentstack.com',
-  password: 'contentstack!@#321',
-  target_stack: 'bltb368d90d5bc390ca',
+  email: '',
+  password: '',
+  target_stack: '',
   // Folder in which exported contents are stored
   data: './New-data'
 };
